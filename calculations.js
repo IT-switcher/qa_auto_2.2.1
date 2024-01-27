1fc0284d84cea4103fd27c4a44e8902f7a6b17a2
Gonshik0001 <kulshaev@yandex.ru>
2024-01-26 23:58:43 -0500

    Edited file calculations.js for homework 2.2.1

--- calculations.js
+++ calculations.js
@@ -1,21 +1,21 @@
 function increaseBalance(x, y) {
-  x = x += y;
+  x += y;
   return x;
 }
 console.log(increaseBalance(3000, 700));
 function decreaseBalance(x,y) {
-x =x-=y;
+x-=y;
 return x;
 }
 console.log(decreaseBalance(3000, 700));
 function divideBalanceByAccounts(x, y) {
-  x = x /= y;
+  x /= y;
   return x;
   console.log(x);
 }
 console.log(divideBalanceByAccounts(3000, 2));
 function getRestAfterDivision(x, y) {
-  x = x%=y;
+  x%=y;
   return x;
 }
 console.log(getRestAfterDivision(7000, 3));
